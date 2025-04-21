ea5df4e4157555321b45a60e0a25dc4a67b0a413
diltormac <146406248+diltormac@users.noreply.github.com>
2025-04-21 16:22:32 -0400

    Update iqbot.py

--- iqbot.py
+++ iqbot.py
@@ -1,1 +1,22 @@
-...
+from iqoptionapi.stable_api import IQ_Option
+import time
+
+class IQBot:
+    def __init__(self, email, senha):
+        self.api = IQ_Option(email, senha)
+
+    def conectar(self):
+        conectado, motivo = self.api.connect()
+        if not conectado:
+            raise Exception(motivo)
+
+        while not self.api.check_connect():
+            time.sleep(1)
+
+        self.api.change_balance("practice")
+        time.sleep(1)
+        return self.api.get_balance()
+
+    def desconectar(self):
+        if self.api:
+            self.api.close()
